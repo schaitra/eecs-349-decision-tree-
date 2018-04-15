b374b3804c38cfc9a9096db2b99c4c0289db480a
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-04-15 14:59:54 -0500

    calculating gain/best attribute

--- Lab1/ID3.py
+++ Lab1/ID3.py
@@ -93,72 +93,113 @@ return entropy
 ## one of their values should be a dict with number of democrats and number of republicans 
 ## I was stuck on how to a)Create this dict and b) how to access different values of it 
 
-def infogain (examples, targetAtt):
-'''
+def infogain(examples, targetAtt):
+    '''
 
-KEEP COUNT OF :
-total amount of each attribute - eg: handicapped - # of yes, # of no, # of q...
-within the particular value of an attribute - #democrat, # republican
-'''
+    KEEP COUNT OF :
+    total amount of each attribute - eg: handicapped - # of yes, # of no, # of q...
+    within the particular value of an attribute - #democrat, # republican
+    '''
 
-# Array of all attributes without 'democrat' or 'republican' (Class)
-att = examples[1].keys()
-del att['Class']
+    # Array of all attributes without 'democrat' or 'republican' (Class)
+    # att = examples[1].keys()
+    # del att['Class']
+    #
+    # l = len(examples)
 
-# Getting classes
-classes = []
-for cls in examples[1].keys()['Class']:
-    classes.append(cls)
 
-countDict = {}
-l = len(examples)
-entropy = 0.0 
-gain = 0.0
+    # for item in examples:
+    #     for attribute in att:
+    #         if value of example[attribute] in dict:
+    #
+    #             count of item in dictionary +1
+    #
+    #             if class of item is already included
+    #                 class +=1
+    #             else:
+    #                 class = 1
+    #         else:
+    #         count of item = 1
+    #         class of item +1
 
-# for item in examples:
-#     for attribute in att:
-#         if value of example[attribute] in dict:
-#
-#             count of item in dictionary +1
-#
-#             if class of item is already included
-#                 class +=1
-#             else:
-#                 class = 1
-#         else:
-#         count of item = 1
-#         class of item +1
+    countDict = {}
 
-#Going through each example
-for example in examples:
-    #Getting all the attributes from that example
-    for attribute, value in example:
-        #Checking if that one attribute has already been looked at for other examples
-        if attribute in countDict.keys():
-            #if it has that attribute, check to see if that value has already been looked at for that attribute for other examples
-            if value in countDict[attribute].keys():
-                #If it has that value, check to see if that value of the targetAtt has been looked at for that value of that attribute for other examples
-                if example[targetAtt] in countDict[attribute][value].keys():
-                    #If that value of that target attribute has already been looked at for this value of this attribute of this example, just incrememt it
-                    countDict[attribute][value][example[targetAtt]] += 1
+    #Going through each example
+    for example in examples:
+        #Getting all the attributes from that example
+        for attribute, value in example:
+            #Checking if that one attribute has already been looked at for other examples
+            if attribute in countDict.keys():
+                #if it has that attribute, check to see if that value has already been looked at for that attribute for other examples
+                if value in countDict[attribute].keys():
+                    #If it has that value, check to see if that value of the targetAtt has been looked at for that value of that attribute for other examples
+                    if example[targetAtt] in countDict[attribute][value].keys():
+                        #If that value of that target attribute has already been looked at for this value of this attribute of this example, just incrememt it
+                        countDict[attribute][value][example[targetAtt]] += 1
 
+                    else:
+                        #Otherwise add that value of that target attribute to the dictionary
+                        countDict[attribute][value][example[targetAtt]] = 1
                 else:
-                    #Otherwise add that value of that target attribute to the dictionary
+                    #Does not have the value, add that value and value of the TargetAtt to the dictionary
                     countDict[attribute][value][example[targetAtt]] = 1
             else:
-                #Does not have the value, add that value and value of the TargetAtt to the dictionary
                 countDict[attribute][value][example[targetAtt]] = 1
-        else:
-            countDict[attribute][value][example[targetAtt]] = 1
 
-#results in a dictionary with {attribute:values}
-#eg: {handicapped infants: y, democrats:, republicans:, n...., ?...}
+    # EXAMPLE OF WHAT THE DICTIONARY ^^^ THAT WOULD CREATE
+    # {{att1:
+    #         {x: {'democrat': 0, 'republican': 0}},
+    #         {y: {'democrat': 5, 'republican': 2}},
+    #         {z: {'democrat': 2, 'republican': 6}},
+    #         {?: {'democrat': 0, 'republican': 1}}},
+    #  {att2:
+    #         {x: {'democrat': 0, 'republican': 0}},
+    #         {y: {'democrat': 5, 'republican': 2}},
+    #         {z: {'democrat': 2, 'republican': 6}},
+    #         {?: {'democrat': 0, 'republican': 1}}}}
 
-for value in dict (y/n/?):
-    for nextvalue in next dict:
-     entropy += nextvalue/(sum of democrats and republicans)*math.log(nextvalue/sum of democrats and republicans)
-     gain += value/len(examples)*entropy 
-     return abs(gain)
+
+    entropy = 0.0
+    gain = 0.0
+    attributeCount = 0
+    valCount = 0
+    best = ""
+
+    for attribute, value in countDict:
+        newGain = 0.0
+        attributeCount =0.0
+        valueResults = []
+        valueTotals = []
+        for val, targetAtts in value:
+            itemCounts = []
+            valTotal = 0.0
+            valResult = 0.0
+            for item, count in targetAtts:
+                valTotal += count
+                itemCounts.append(count)
+
+            for num in itemCounts:
+                valResult += (num/valTotal) * math.log((num/valTotal),2)
+
+            attributeCount += valCount
+            valueResults.append(valResult)
+            valueTotals.append(valTotal)
+
+        for counter, num in enumerate(valueResults):
+            newGain += (valueTotals[counter]/len(examples)) * num
+
+        if newGain > gain:
+            gain = newGain
+            best = attribute
+
+    return best
+
+
+# for value in dict (y/n/?):
+#     for nextvalue in next dict:
+#      entropy += nextvalue/(sum of democrats and republicans)*math.log(nextvalue/sum of democrats and republicans)
+#      gain += value/len(examples)*entropy
+#      return abs(gain)
 
 
 
@@ -175,6 +216,13 @@ def test(node, examples):
   Takes in a trained tree and a test set of examples.  Returns the accuracy (fraction
   of examples the tree classifies correctly).
   '''
+  count = 0.0
+  for example in examples:
+      exclass = evaluate(node,example)
+      if exclass  == example['Class'].value():
+        count+=1
+    acc = count/len(examples)
+  return acc
 
 
 def evaluate(node, example):
